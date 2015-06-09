2a076912d9c092c1cfe353febac318062cb82de7
Akiva <akiva@neko.local>
2015-06-09 12:49:20 -0500

    Modernizes project.clj and updates dependencies.

--- project.clj
+++ project.clj
@@ -1,15 +1,27 @@
-(defproject xyzzwhy "0.7.0-SNAPSHOT"
+(defproject xyzzwhy "0.8.0-SNAPSHOT"
   :description "A Twitter bot who takes you on surreal adventures."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/akivaschoen/xyzzwhy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [environ "1.0.0"]
-                 [com.novemberain/monger "2.0.0"]
-                 [twitter-api "0.7.7"]
-                 [typographer "1.0.0"]]
+
+  :min-lein-version "2.5.0"
+
+  :dependencies
+  [[org.clojure/clojure "1.7.0-RC1"]
+   [environ "1.0.0"]
+   [com.novemberain/monger "2.1.0"]
+   [twitter-api "0.7.8"]
+   [typographer "1.1.0"]]
+
   :plugins [[lein-environ "1.0.0"]]
-  :main ^:skip-aot xyzzwhy.core
+
+  :main xyzzwhy.core
+
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
-  :min-lein-version "2.0.0")
+
+  :profiles
+  {:uberjar {:aot :all}
+
+   :dev [:database :twitter
+         {:env {:dev true}
+          :jvm-opts ^:replace ["-XX:-OmitStackTraceInFastThrow"]}]})
