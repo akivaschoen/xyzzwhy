1ca57d16a441c2579e3612f7b9ac9eb962399231
Akiva <akiva@neko.local>
2015-07-11 08:32:12 -0500

    Updates project with latest dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xyzzwhy "0.8.0-SNAPSHOT"
+(defproject xyzzwhy "2.0.0-SNAPSHOT"
   :description "A Twitter bot who takes you on surreal adventures."
   :url "https://github.com/akivaschoen/xyzzwhy"
   :license {:name "Eclipse Public License"
@@ -7,21 +7,34 @@
   :min-lein-version "2.5.0"
 
   :dependencies
-  [[org.clojure/clojure "1.7.0-RC1"]
+  [[org.clojure/clojure "1.7.0"]
    [environ "1.0.0"]
-   [com.novemberain/monger "2.1.0"]
+   [com.novemberain/monger "3.0.0-rc2"]
    [twitter-api "0.7.8"]
    [typographer "1.1.0"]]
 
-  :plugins [[lein-environ "1.0.0"]]
+  :plugins [[lein-autoexpect "1.6.0"]
+            [lein-environ "1.0.0"]]
 
   :main xyzzwhy.core
 
   :target-path "target/%s"
 
   :profiles
-  {:uberjar {:aot :all}
+  {:uberjar {:env {:production true}
+             :omit-source true
+             :aot :all}
 
-   :dev [:database :twitter
+   :prod [:prod-config :twitter
+          {:env {:production true}
+           :omit-source true
+           :aot :all}]
+
+   :dev [:dev-config :twitter
          {:env {:dev true}
+
+          :dependencies
+          [[expectations "2.0.16"]
+           [leiningen "2.5.1"]]
+
           :jvm-opts ^:replace ["-XX:-OmitStackTraceInFastThrow"]}]})
