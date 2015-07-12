3d81c995947e292e9ea4e3ac5b7c705d590554b7
Akiva <akiva@neko.local>
2015-07-12 11:53:24 -0500

    Makes tweet production fully composable.

--- src/xyzzwhy/engine.clj
+++ src/xyzzwhy/engine.clj
@@ -1,6 +1,9 @@
 (ns xyzzwhy.engine
   (:require [clojure.string :as str]
+            [typographer.core :as typo]
             [xyzzwhy.text :refer :all]))
+
+(def corpus)
 
 (defn random-pick
   [coll]
@@ -26,7 +29,8 @@
 
 (defn prep
   [fragment]
-  (-> (:preps fragment) random-pick pad))
+  (if-let [preps (contains? fragment :preps)]
+    (-> preps random-pick pad)))
 
 (defn no-groups?
   [config]
@@ -64,22 +68,47 @@
 
 (defn append
   [fragment text]
-  (update fragment :text #(str %1 " " text)))
+  (update fragment :text #(str %1 text)))
 
 (defn defcorpus
   [classes]
   (reduce #(conj %1 @(-> %2 symbol resolve)) {} classes))
 
-(defn get-class
-  [corpus class]
-  (-> class name (str "s") keyword corpus))
+(defn interpolate
+  ([fragment]
+   (reduce #(interpolate %1 %2) fragment (:subs fragment)))
+  ([fragment sub]
+   (let [sub' (val sub)
+         prep' (when (prep? (:config sub'))
+                 (-> sub' :source prep))
+         article' (when (article? (:config sub'))
+                    (-> sub' :source article))
+         text (str prep' article' (-> sub' :source :text))]
+     (assoc fragment :text
+            (str/replace (:text fragment) (str "%" (key sub)) text)))))
 
-(defmulti get-fragment* (fn [_ class _] class))
+(defn classname
+  [c]
+  (let [c' (name c)]
+    (if (.endsWith c' "s")
+      (keyword c')
+      (keyword (str c' "s")))))
+
+(defn get-class
+  [c]
+  (-> c classname corpus))
+
+(defn choose-event
+  []
+  (-> corpus :events random-pick))
+
+(defmulti get-fragment* (fn [c _] c))
 
 (defmethod get-fragment* :actor
-  [corpus class config]
-  (let [persons (->> :person (get-class corpus))
-        animals (->> :animal (get-class corpus))
+  [c config]
+  (println c)
+  (let [persons (get-class :persons)
+        animals (get-class :animals)
         actors (apply merge persons animals)
         actors (if (no-groups? config)
                  (filter #(not= :group (-> % :gender)) actors)
@@ -87,72 +116,80 @@
     (-> actors random-pick)))
 
 (defmethod get-fragment* :default
-  [corpus class config]
-  (->> class (get-class corpus) random-pick))
+  [c config]
+  (println c)
+  (->> c classname corpus random-pick))
 
 (defn get-fragment
-  ([corpus class]
-   (get-fragment* corpus class nil))
-  ([corpus class config]
-   (get-fragment* corpus class config)))
+  ([c]
+   (get-fragment c nil))
+  ([c config]
+   (get-fragment* c config)))
 
-(defmulti get-sub (fn [_ _ sub] (-> sub val :class)))
+(defmulti get-sub (fn [_ sub] (-> sub val :class)))
 
 (defmethod get-sub :gender
-  [_ fragment sub]
+  [fragment sub]
   (let [sub' (val sub)
-        sex (-> fragment :subs (find (key sub)) val :source :gender)]
-    {(key sub) (assoc-in sub' [:source :text] (gender sex (:case sub')))}))
+        gender' (-> fragment :subs (find (key sub)) val :source :gender)]
+    {(key sub) (assoc-in sub' [:source :text] (gender gender' (:case sub')))}))
 
 (defmethod get-sub :default
-  [corpus fragment sub]
+  [fragment sub]
   (let [sub' (val sub)]
-    {(key sub) (assoc sub' :source (get-fragment corpus (-> sub' :class)))}))
-
-(defn get-subs*
-  [corpus fragment subs]
-  (reduce #(conj %1 (get-sub corpus fragment %2)) {} subs))
+    {(key sub) (assoc sub' :source (get-fragment (-> sub' :class)))}))
 
 (defn get-subs
-  ([corpus fragment]
-   (let [subs (get-subs* corpus fragment (:subs fragment))]
+  [fragment subs]
+  (reduce #(conj %1 (get-sub fragment %2)) {} subs))
+
+(defn subs
+  ([fragment]
+   (let [subs (get-subs fragment (:subs fragment))]
      (assoc fragment :subs subs)))
-  ([corpus fragment follow-up]
-   (let [subs (get-subs* corpus fragment (:subs follow-up))
+  ([fragment follow-up]
+   (let [subs (get-subs fragment (:subs follow-up))
          follow-up' (assoc follow-up :subs subs)]
      follow-up')))
 
-(defmulti get-follow-up* (fn [_ _ follow-up _] (contains? follow-up :subs)))
+(defmulti get-follow-up* (fn [_ follow-up _] (contains? follow-up :subs)))
 
 (defmethod get-follow-up* false
-  [corpus fragment follow-up _]
+  [fragment follow-up _]
   (append fragment (:text follow-up)))
 
 (defmethod get-follow-up* true
-  [corpus fragment follow-up index]
-  (let [option (get-subs corpus fragment follow-up)
+  [fragment follow-up index]
+  (let [option (get-subs fragment follow-up)
         option (interpolate option)
         fragment (append fragment (:text option))]
     (assoc-in fragment [:follow-ups :options index] option)))
 
-(defn follow-up
-  [corpus fragment]
-  (let [options (-> fragment :follow-ups :options)
-        follow-up (-> options random-pick)
-        index (.indexOf options follow-up)]
-    (get-follow-up* corpus fragment follow-up index)))
+(defn follow-ups
+  [fragment]
+  (if (contains? fragment :follow-ups)
+    (let [options (-> fragment :follow-ups :options)
+          follow-up (-> options random-pick)
+          index (.indexOf options follow-up)]
+      (get-follow-up* fragment follow-up index))
+    fragment))
 
-(defn interpolate
-  ([fragment]
-   (reduce #(interpolate %1 %2) fragment (:subs fragment)))
-  ([fragment sub]
-  (let [sub' (val sub)
-        prep' (when (prep? (:config sub'))
-                (-> sub' :source :prep random-pick))
-        article' (when (article? (:config sub'))
-                   (-> sub' :source article))
-        text (str prep' article' (-> sub' :source :text))]
-    (assoc fragment :text
-           (str/replace (:text fragment) (str "%" (key sub)) text)))))
+(defn dot-prefix
+  [fragment]
+  (update fragment :text #(str/replace % #"^(@\w+)" ".$1")))
 
-(def construct-tweet (comp interpolate follow-up))
+(defn capitalize*
+  [fragment]
+  (assoc fragment :text (-> (:text fragment)
+                            (str/replace #"^[a-z]+" #(str/capitalize %1))
+                            (str/replace #"(\.\s)([a-z]+)"
+                                            #(str (second %1)
+                                                  (str/capitalize (nth %1 2)))))))
+
+(defn smarten*
+  [fragment]
+  (update fragment :text #(typo/smarten %)))
+
+(def initialize-tweet (comp get-fragment classname choose-event))
+(def process-tweet (comp interpolate follow-ups subs))
+(def finalize-tweet (comp smarten* dot-prefix capitalize*))
