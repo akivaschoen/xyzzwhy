cabfa37a6b9782893450da58bb3803cebc83aac8
Akiva <akiva@akivamail.com>
2016-02-04 11:59:51 -0600

    Adds preferential config merging.

--- src/xyzzwhy/engine/config.clj
+++ src/xyzzwhy/engine/config.clj
@@ -1,13 +1,23 @@
 (ns xyzzwhy.engine.config
   (:require [clojure.string :as str]))
 
-(defn config-complement
+(defn option-complement
   [c]
   (let [s (name c)]
     (if (str/starts-with? s "no-")
       (keyword (str/replace s #"^no-" ""))
       (keyword (str "no-" s)))))
 
-(defn has-complement?
-  [s c]
-  (contains? v (config-complement c)))
+(defn option-complement?
+  [option config]
+  (contains? config (option-complement option)))
+
+(defn merge-configs
+  "Merges c1 into c2 with c2 taking precedence."
+  [c1 c2]
+  (reduce (fn [acc opt]
+            (if (option-complement? opt c2)
+              acc
+              (conj acc opt)))
+          c2
+          c1))
