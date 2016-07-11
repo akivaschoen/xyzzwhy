4dbde63ecaa181cfe7df43d1733fad70b41563c1
Akiva <akiva@akivamail.com>
2016-07-10 21:09:08 -0500

    Refactor handling of substitutions and interpolations.

--- src/xyzzwhy/engine.clj
+++ src/xyzzwhy/engine.clj
@@ -1,9 +1,10 @@
 (ns xyzzwhy.engine
   (:require [xyzzwhy.engine
              [fragment :as fr]
-             [interpolation :refer :all]
+             [interpolation :as in]
              [substitution :as sb]]
-            [xyzzwhy.util :as util]))
+            [xyzzwhy.util :as util]
+            [clojure.string :as str]))
 
 (def tweetmap
   {:tweet nil
@@ -17,3 +18,19 @@
   [tweetmap]
   (let [fragment (fr/fragment (get-in tweetmap [:event :name]))]
     (update tweetmap :event merge fragment)))
+
+(defn tweet-text
+  [tweetmap]
+  (assoc tweetmap :tweet (get-in tweetmap [:event :text])))
+
+(defn substitutes
+  [tweetmap]
+  (update-in tweetmap [:event :sub] #(mapv sb/substitute %)))
+
+(defn transclude
+  [tweetmap]
+  (let [tweet-text (reduce (fn [text sub]
+                             (in/interpolate text sub))
+                           (:tweet tweetmap)
+                           (get-in tweetmap [:event :sub]))]
+    (assoc tweetmap :tweet tweet-text)))
--- src/xyzzwhy/engine/interpolation.clj
+++ src/xyzzwhy/engine/interpolation.clj
@@ -13,7 +13,7 @@
   (when (fr/article? s)
     (fr/article s)))
 
-(defn interpolate
+(defn interpolate*
   #_([fragment]
    (reduce #(interpolate %1 %2) fragment (:sub fragment)))
   ([fragment item]
@@ -21,3 +21,10 @@
                    (article item)
                    (:text item))] 
      (update fragment :text str/replace (str "%" (:token item)) text))))
+
+(defn interpolate
+  [tweet-text sub]
+  (let [text (str (prep sub)
+                  (article sub)
+                  (:text sub))] 
+    (str/replace tweet-text (str "%" (:token sub)) text)))
