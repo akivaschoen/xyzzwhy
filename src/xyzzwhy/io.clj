ef94a3e7d3a6590863bd1d1847867965b0ec4763
Akiva <akiva@akivamail.com>
2016-07-19 21:37:40 -0500

    Add basic EDN read-file functionality.

---
+++ src/xyzzwhy/io.clj
@@ -0,0 +1,26 @@
+(ns xyzzwhy.io
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io])
+  (:import java.io.PushbackReader))
+
+(defonce corpora-dir "resources/corpora/")
+
+;;
+;; EDN Functions
+;; (Thanks to yogthos)
+;;
+(defn edn-prefix
+  [s]
+  (if (str/ends-with? s ".edn")
+    s
+    (str s ".edn")))
+
+(defn read-file
+  [filename]
+  (let [path (str corpora-dir (edn-prefix filename))]
+    (try
+      (with-open [file (-> path io/reader PushbackReader.)]
+        (edn/read file))
+      (catch Exception e
+        (println (str "OH GOD. Failed to load '" path "': " (.getLocalizedMessage e)))))))
