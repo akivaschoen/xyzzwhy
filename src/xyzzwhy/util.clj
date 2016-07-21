fc372ee778e5f0861acdf03c247d927ce35374f8
Akiva <akiva@akivamail.com>
2016-07-21 13:52:13 -0500

    Refactor util and add tests.

--- src/xyzzwhy/util.clj
+++ src/xyzzwhy/util.clj
@@ -1,5 +1,5 @@
 (ns xyzzwhy.util
-  (:require [clojure.string :as str]
+  (:require [clojure.string :as cstr]
             [typographer.core :as typo]))
 
 (def any? (complement not-any?))
@@ -7,20 +7,11 @@
 (defn append
   "Adds text to fragment's :text and returns fragment."
   [oldtext newtext]
-  (if (< (+ (-> oldtext count)
+  (if (< (+ (count oldtext)
             (count newtext))
          139)
     (str oldtext newtext)
     oldtext))
-
-(defn capitalize-first
-  "Capitalizes fragment's sentences."
-  [fragment]
-  (assoc fragment :text (-> (:text fragment)
-                            (str/replace #"^[a-z]+" #(str/capitalize %1))
-                            (str/replace #"(\.\s)([a-z]+)"
-                                            #(str (second %1)
-                                                  (str/capitalize (nth %1 2)))))))
 
 (defn chance
   "Returns true if a randomly chosen percentile is less
@@ -32,34 +23,10 @@
      true
      false)))
 
-(defn prefix-dot
-  "Prefix's fragment's :text with a period if it begins with
-  an @mention."
-  [fragment]
-  (update fragment :text #(str/replace % #"^(@\w+)" ".$1")))
-
-(defn format-text
-  "Applies a preposition and/or an article to a given fragment."
-  [thing]
-  (let [text    (:text thing)
-        article (:article thing)
-        prep    (:prep thing)
-        config  (:config thing)]
-    (cond->> text
-      (and (not-empty article)
-           (not-any? #(= :no-article %) config))  (str article " ")
-      (and (not-empty prep)
-           (not-any? #(= :no-prep %) config))     (str (rand-nth prep) " "))))
-
-(defn optional?
-  "Returns true if a follow-up is optional."
-  [follow-up]
-  (-> follow-up :optional? true?))
-
 (defn smarten
   "Converts fragment's text to use typographer's quotes."
-  [fragment]
-  (update fragment :text #(typo/smarten %)))
+  [text]
+  (typo/smarten text))
 
 (defn pick
   "Given a vector, randomly chooses one item; if a string,
@@ -76,3 +43,19 @@
   (when (vector? c)
     (let [item (rand-nth c)]
       (first {(.indexOf c item) item}))))
+
+(defn prefix
+  "Prefix's fragment's :text with a period if it begins with
+  an @mention."
+  [text]
+  (cstr/replace text #"^(@\w+)" ".$1"))
+
+(defn sentence-case
+  "Capitalizes fragment's sentences."
+  [text]
+  (-> text
+      (cstr/replace #"^[a-z]+" cstr/capitalize)
+      (cstr/replace #"(\.\s)([a-z]+)" #(str (second %1)
+                                            (cstr/capitalize (nth %1 2))))))
+
+(def finalize (comp smarten prefix sentence-case))
