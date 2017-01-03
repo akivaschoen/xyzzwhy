5ae5f9f463b5f5f168302bcd6ca06d4e926610d4
Akiva <akiva@akivamail.com>
2017-01-03 13:09:02 -0800

    Sets correctly named environment settings.

--- src/xyzzwhy/twitter.clj
+++ src/xyzzwhy/twitter.clj
@@ -2,14 +2,14 @@
   (:require [environ.core :refer [env]]
             [twitter.api.restful :refer :all]
             [twitter.oauth :refer :all])
-  (:import (twitter.callbacks.protocols SyncSingleCallback)))
+  (:import [twitter.callbacks.protocols SyncSingleCallback]))
 
 (def credentials
   (make-oauth-creds
-    (env :twitter-consumer-key)
-    (env :twitter-consumer-secret)
-    (env :twitter-user-access-token)
-    (env :twitter-user-access-token-secret)))
+    (env :xyzzwhy-twitter-consumer-key)
+    (env :xyzzwhy-twitter-consumer-secret)
+    (env :xyzzwhy-twitter-user-access-token)
+    (env :xyzzwhy-twitter-user-access-token-secret)))
 
 (defn update
   "This, uh, posts to Twitter."
