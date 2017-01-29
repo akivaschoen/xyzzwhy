ff805118b6604abecaf09069179161263c89053a
Akiva <akiva@akivamail.com>
2017-01-29 11:07:40 -0800

    Updates to handle vec/map based classes.edn.

--- src/xyzzwhy/corpora.clj
+++ src/xyzzwhy/corpora.clj
@@ -2,11 +2,15 @@
   (:require [xyzzwhy.io :as io]
             [xyzzwhy.util :as util]))
 
+(defn index-of
+  [idx c]
+  (some #(if (= idx (:index %)) %) c))
+
 (defn weighted-pick
   [c]
-  (let [weight (reductions #(+ %1 %2) (vals c))
+  (let [weight (reductions #(+ %1 %2) (map :weight c))
         rnd (rand-int (last weight))]
-    (nth (keys c) (count (take-while #(<= % rnd) weight)))))
+    (nth c (count (take-while #(<= % rnd) weight)))))
 
 (defn get-config
   [classname]
@@ -14,8 +18,11 @@
       {:config #{}}))
 
 (defn get-event
-  []
-  {:class (weighted-pick (:events (io/read-file "classes")))})
+  ([]
+   {:class (weighted-pick (:events (io/read-file "classes")))})
+  ([index]
+   (let [index (find (:events (io/read-file "classes")) )])
+   {:class (find (:events (io/read-file "classes")))}))
 
 (defn get-fragments
   [classname]
