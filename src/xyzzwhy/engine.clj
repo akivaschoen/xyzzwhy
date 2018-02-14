170eff500a3f047944bf0b053a2b05d611443dc6
Akiva <akiva@akivamail.com>
2018-02-13 18:32:14 -0600

    Adds full appending of random :event & possible follow-ups.

--- src/xyzzwhy/engine.clj
+++ src/xyzzwhy/engine.clj
@@ -1,14 +1,20 @@
-(ns xyzzwhy.engine
-  (:require [xyzzwhy.io :as io]
-            [xyzzwhy.util :as util]))
+ (ns xyzzwhy.engine
+   (:require [xyzzwhy.configure :as conf]
+             [xyzzwhy.fragment :as fr]
+             [xyzzwhy.io :as io]
+             [xyzzwhy.util :as util]))
 
 (def tweetmap
   {:tweet ""
    :group nil
    :config #{[:gender :neutral]}
+   :event []
+   :secondary []
+   :tertiary []
    :fragments []})
 
 (defn initialize []
+  "Returns a fresh tweetmap configured with a random :group."
   (assoc tweetmap :group (-> (io/read-file :group)
                              :events
                              util/weighted-nth
--- src/xyzzwhy/fragment.clj
+++ src/xyzzwhy/fragment.clj
@@ -3,27 +3,43 @@
             [xyzzwhy.configure :as conf]
             [xyzzwhy.util :as util]))
 
-(defn follow-up? [fragment]
-  "50% chance of returning a random follow-up to a fragment, should
-it have any."
-  (and (util/has? fragment :follow-up)
-       (or (conf/required? (-> fragment :follow-up :config))
-           (util/chance))))
-
-(defn follow-up [fragment]
-  (-> fragment
-      :follow-up
-      :fragment
-      rand-nth))
-
-(defn select-fragment [m]
+(defn select [m]
+  "Returns a random item from the :fragment key in m."
   (rand-nth (:fragment m)))
 
-(defn select-group [m]
+(defn follow-up? [tmap & paths]
+  "50% chance of returning a random follow-up to a fragment, should
+it have any."
+  (let [paths' (apply conj [] paths)]
+    (if (util/has? (get-in tmap paths') :follow-up)
+      (or (conf/required? (get-in tmap (conj paths' :follow-up :config)))
+          (util/chance))
+      false)))
+
+(defn follow-up [tmap & paths]
+  "Appends a random :follow-up to :fragments."
+  (let [paths' (conj (apply conj [] paths) :follow-up) 
+        text (select (get-in tmap paths'))]
+    (update tmap :fragments #(reduce conj % (:text text)))))
+
+(defn group [m]
+  "Returns a :group's possible entries."
   (-> (:group m)
       (io/read-file)))
 
-(defn append [tmap fragment]
-  (update tmap :fragments conj (first (:text fragment))))
+(defn append [tmap & paths]
+  "Appends an entry on keyword paths (e.g., :event :text) to a tmap's
+:fragments key."
+  (let [path (apply conj [] paths)]
+    (update tmap :fragments #(reduce conj % (get-in tmap path)))))
 
+(defn select-event [tmap]
+  "Configures an event for a newly initialized tweetmap and appends an
+event's :text to the tmap's :fragments."
+  (let [select-from-group (comp select group)
+        tmap' (-> (assoc tmap :event (select-from-group tmap))
+                  (append :event :text))]
+    (if (follow-up? tmap' :event)
+      (follow-up tmap' :event)
+      tmap')))
 
